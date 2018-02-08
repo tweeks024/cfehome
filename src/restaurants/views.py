1eeef06ca16b9749be3fb6e3bb758fd3b34a4240
tweeks <tweeks@timetrade.com>
2018-02-08 18:29:17 -0500

    add class based views

--- src/restaurants/views.py
+++ src/restaurants/views.py
@@ -1,10 +1,28 @@
 import random
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.views import View
+from django.views.generic import TemplateView
 
 # Create your views here.
 # function based view
 
-def home(request):
-    num = random.randint(0, 100000)
-    return render(request, "base.html", {"html_var": "some context variable", "num": num})
+class HomeView(TemplateView):
+    template_name = 'home.html'
+
+    def get_context_data(self, *args, **kwargs):
+        context = super(HomeView, self).get_context_data(*args, **kwargs)
+        num = None
+        some_list = [
+            random.randint(0,1000000),
+            random.randint(0,1000000),
+            random.randint(0,1000000)
+        ]
+        condition_bool_item = True
+        if condition_bool_item:
+            num = random.randint(0, 100000)
+        context = {
+            "num": num,
+            "some_list": some_list
+        }
+        return context
