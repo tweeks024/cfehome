24a25f85b3d74e53359d02de93ee29a78b3d21b4
tweeks <tweeks@timetrade.com>
2018-02-08 18:45:48 -0500

    add query sets

--- src/restaurants/views.py
+++ src/restaurants/views.py
@@ -2,27 +2,13 @@ import random
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.views import View
-from django.views.generic import TemplateView
 
-# Create your views here.
-# function based view
+from .models import RestaurantLocation
 
-class HomeView(TemplateView):
-    template_name = 'home.html'
-
-    def get_context_data(self, *args, **kwargs):
-        context = super(HomeView, self).get_context_data(*args, **kwargs)
-        num = None
-        some_list = [
-            random.randint(0,1000000),
-            random.randint(0,1000000),
-            random.randint(0,1000000)
-        ]
-        condition_bool_item = True
-        if condition_bool_item:
-            num = random.randint(0, 100000)
-        context = {
-            "num": num,
-            "some_list": some_list
-        }
-        return context
+def restaurant_listview(request):
+    template_name = 'restaurants/restaurants_list.html'
+    query_set = RestaurantLocation.objects.all()
+    context = {
+        "object_list": query_set
+    }
+    return render(request, template_name, context)
