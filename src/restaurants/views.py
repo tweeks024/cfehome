983ab5e5d9cbceb0e799c498120845acd0681135
Tom Weeks <tweeks@timetrade.com>
2018-02-12 15:29:40 -0500

    add forms

--- src/restaurants/views.py
+++ src/restaurants/views.py
@@ -3,25 +3,23 @@ from django.db.models import Q
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 from django.views import View
-from django.views.generic import TemplateView, ListView, DetailView
+from django.views.generic import TemplateView, ListView, DetailView, CreateView
 
-from .forms import RestaurantCreateForm
+from .forms import RestaurantCreateForm, RestaurantLocationCreateForm
 from .models import RestaurantLocation
 
 
 def restaurant_createview(request):
-    if request.method == "POST":
-        title = request.POST.get("title") #title = request.POST["title"]
-        location = request.POST.get("location")
-        category = request.POST.get("category")
-        obj = RestaurantLocation.objects.create(
-            name = title,
-            location = location,
-            category = category
-            )
+    form = RestaurantLocationCreateForm(request.POST or None)
+    errors = None
+    if form.is_valid():
+        form.save()
         return HttpResponseRedirect("/restaurants/")
+    if form.errors:
+        print(form.errors)
+        errors = form.errors
     template_name = 'restaurants/form.html'
-    context = {}
+    context = {"form": form, "errors": errors}
     return render(request, template_name, context)
 
 
@@ -61,13 +59,7 @@ class RestaurantListView(ListView):
 class RestaurantDetailView(DetailView):
     queryset = queryset = RestaurantLocation.objects.all()
 
-    def get_context_data(self, *args, **kwargs):
-        print(self.kwargs)
-        context = super(RestaurantDetailView, self).get_context_data(*args, **kwargs)
-        print(context)
-        return context
-    #
-    # def get_object(self, *args, **kwargs):
-    #     rest_id = self.kwargs.get('rest_id')
-    #     obj = get_object_or_404(RestaurantLocation, id=rest_id)
-    #     return obj
+class RestaurantCreateView(CreateView):
+    form_class = RestaurantLocationCreateForm
+    template_name = 'restaurants/form.html'
+    success_url = "/restaurants/"
